86c6ff7b611e79c035f1a484aac7285bb5feeaac
Vadim Demedes <vadimdemedes@hey.com>
2023-06-09 18:42:57 +0300

    Init

---
+++ readme.md
@@ -0,0 +1,87 @@
+# astro-selfie
+
+> [Astro](https://astro.build) integration to generate page screenshots to show as Open Graph images.
+
+## Install
+
+```console
+npm install --save-dev astro-selfie
+```
+
+## Usage
+
+### 1. Set up integration
+
+Add this integration to `astro.config.mjs`:
+
+```diff
+import {defineConfig} from 'astro/config';
++ import selfie from 'astro-selfie';
+
+export default defineConfig({
++    integrations: [
++        // Make sure `astro-selfie` only runs locally
++        !process.env['CI'] && !process.env['VERCEL'] && selfie()
++    ].filter(Boolean)
+});
+```
+
+This integration is meant to be used locally for statically built websites for several reasons:
+
+1. Websites deployed to Vercel don't have access to headless Chrome due to platform limitations.
+2. Open graph images aren't probably useful in continuous integration.
+3. Taking screenshots is not quick.
+
+### 2. Add meta tags
+
+Then, add a `<meta>` tag to each page that points to a screenshot of itself.
+
+```jsx
+---
+import {selfieUrl} from 'astro-selfie';
+
+const screenshotUrl = selfieUrl(Astro);
+---
+<meta
+	property="og:image"
+    content={screenshotUrl.href}
+/>
+```
+
+### 3. Customize styles (optional)
+
+Selfie adds a `data-astro-selfie` attribute to `body` when taking a screenshot. You can use that data attribute to change any styles in CSS to make sure page looks good.
+
+For example:
+
+```css
+body[data-astro-selfie] .container {
+	padding: 32px 64px;
+}
+```
+
+### 4. Generate screenshots
+
+Run a build command to take screenshots of all pages and store them in `public/og` directory.
+
+```console
+npx astro build
+```
+
+Once screenshots are generated, commit them to version control and deploy.
+
+## API
+
+### selfie()
+
+Returns an Astro integration that takes page screenshots.
+
+### selfieUrl(astro): URL
+
+Returns a URL to the screenshot of the current page.
+
+#### astro
+
+Type: `AstroGlobal`
+
+Global `Astro` object.
---
+++ source/index.ts
@@ -0,0 +1,95 @@
+import path from 'node:path';
+import http from 'node:http';
+import {fileURLToPath} from 'node:url';
+import fs from 'node:fs/promises';
+import getPort from 'get-port';
+import serveHandler from 'serve-handler';
+import {serve} from 'micro';
+import {chromium} from 'playwright';
+import makeDir from 'make-dir';
+import type {AstroGlobal, AstroIntegration} from 'astro';
+
+export default function selfie(): AstroIntegration {
+	let publicDir: URL;
+
+	return {
+		name: 'astro-selfie',
+		hooks: {
+			// eslint-disable-next-line @typescript-eslint/naming-convention, object-shorthand
+			'astro:config:done': ({config}) => {
+				publicDir = (config as unknown as {publicDir: URL}).publicDir;
+			},
+			// eslint-disable-next-line @typescript-eslint/naming-convention, object-shorthand
+			'astro:build:done': async ({dir, pages}) => {
+				const screenshotsDir = new URL('og', publicDir);
+				await makeDir(fileURLToPath(screenshotsDir));
+
+				const port = await getPort();
+				const baseUrl = new URL(`http://localhost:${port}`);
+
+				const server = new http.Server(
+					serve(async (request, response) => {
+						await serveHandler(request, response, {
+							public: fileURLToPath(dir),
+						});
+					}),
+				);
+
+				server.listen(port);
+
+				const browser = await chromium.launch();
+
+				const context = await browser.newContext({
+					screen: {
+						width: 1200,
+						height: 600,
+					},
+					viewport: {
+						width: 1200,
+						height: 600,
+					},
+				});
+
+				for (const {pathname} of pages) {
+					const url = new URL(pathname, baseUrl);
+					const page = await context.newPage();
+					await page.goto(url.href);
+
+					await page.evaluate(
+						'document.body.setAttribute("data-astro-selfie", "true")',
+					);
+
+					const screenshot = await page.screenshot({type: 'png'});
+
+					const screenshotPath = path.join(
+						fileURLToPath(screenshotsDir),
+						pathname === '' ? 'index.png' : `${pathname}.png`,
+					);
+
+					await makeDir(path.dirname(screenshotPath));
+					await fs.writeFile(screenshotPath, screenshot);
+				}
+
+				await browser.close();
+				server.close();
+			},
+		},
+	};
+}
+
+const stripTrailingSlash = (input: string): string => {
+	return input.replace(/\/$/, '');
+};
+
+const selfiePath = (astro: AstroGlobal): string => {
+	const pathname =
+		astro.url.pathname === '/'
+			? '/index'
+			: stripTrailingSlash(astro.url.pathname);
+
+	return `/og${pathname}.png`;
+};
+
+export const selfieUrl = (astro: AstroGlobal): URL => {
+	return new URL(selfiePath(astro), astro.site);
+};
